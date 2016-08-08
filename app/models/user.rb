7d1993ec0d16879e06232ab3d4052db1b1adccf0
Marie-Elise <membrq@gmail.com>
2016-08-08 08:08:26 -0500

    Checkpoint 37 Lesson

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,7 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: {minimum: 6}, unless: :password_digest
   validates :password, length: {minimum: 6}, allow_blank: true
 
-  validates :email,
-            presence: true,
-            uniqueness: {case_sensitive: false},
-            length: {minimum: 3, maximum: 254}
+  validates :email, presence: true, uniqueness: {case_sensitive: false}, length: {minimum: 3, maximum: 254}
 
   has_secure_password
 end
