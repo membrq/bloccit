0660eacb974ba30f59684da46ddc90c5f633a2b3
Marie-Elise <membrq@gmail.com>
2016-08-23 21:58:02 -0500

    Lesson 48 WIP

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160818042143) do
+ActiveRecord::Schema.define(version: 20160824014143) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -108,6 +108,7 @@ ActiveRecord::Schema.define(version: 20160818042143) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "role"
+    t.string   "auth_token"
   end
 
   create_table "votes", force: :cascade do |t|
